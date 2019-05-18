4a8fe9e0ee40db73ab011a130d1426144bc0d6cd
JuniorArruda <juniorarruda15@gmail.com>
2019-05-18 19:01:33 -0300

    Exercio 04 comluido

---
+++ lista-de-exercicios-01/fatorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int num, fat;
+	printf("Digite o numero que deseja ser fatorado: \n");
+	scanf(%d, &num);
+
+	for(fat = 1; n > 1; n - 1){
+		fat = fat * n;
+	}
+
+	printf("\nfatorial do numero %d é = %d ",num,fat);
+
+	return 0;
+	system("pause");
+}
